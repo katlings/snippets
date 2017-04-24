3452071c3ede6ae9a219a69a3b6de94d50c6be3f
Kathryn Lingel <klingel@indeed.com>
2017-04-24 15:45:47 -0700

    quality of life updates

--- lifehacks/playlist_creator/make_playlist.py
+++ lifehacks/playlist_creator/make_playlist.py
@@ -12,8 +12,9 @@ from gmusicapi import Mobileclient
 
 
 class PlaylistMaker(object):
-    def __init__(self):
+    def __init__(self, dry_run=False):
         self.api = Mobileclient()
+        self.live = not dry_run
 
         with open('creds.json') as f:
             creds = json.loads(f.read())
@@ -21,24 +22,35 @@ class PlaylistMaker(object):
         self.logged_in = self.api.login(creds['username'], creds['password'], Mobileclient.FROM_MAC_ADDRESS)
 
     def make_playlist(self, name):
-        return self.api.create_playlist(name, public=True)
+        return self.api.create_playlist(name, public=True) if self.live else "dry-run-no-playlist"
 
     def populate_playlist(self, playlist_id, songs):
         song_ids = []
+        not_found_songs = []
         
         for song in songs:
             result = self.api.search(song)
-            hit = result.get('song_hits')[0]['track']
-            print "Adding: ", hit['title'], " - ", hit['artist']
-            song_ids.append(hit['storeId'])
+            try:
+                hit = result.get('song_hits')[0]['track']
+                song_ids.append(hit['storeId'])
+                print "Adding: ", hit['title'], "-", hit['artist']
+            except IndexError, KeyError:
+                not_found_songs.append(song)
+                print "Could not find a result for: ", song
 
-        self.api.add_songs_to_playlist(playlist_id, song_ids)
+        if self.live:
+            self.api.add_songs_to_playlist(playlist_id, song_ids)
+            print "Added {} songs to playlist".format(len(song_ids))
+
+        print 'Songs not found:'
+        print '\n'.join(not_found_songs)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Make a playlist and populate with songs. Store credentials in creds.json with format {"username": username, "password": password}')
     parser.add_argument('--filename', '-f', required=True, help='Text file full of search terms. One song per line will be added to the playlist')
     parser.add_argument('--name', '-n', help='Name of the playlist. Defaults to the filename')
+    parser.add_argument('--dry-run', '-d', action='store_true', help='Just print search results; don\'t actually create a playlist or add songs to it')
     args = parser.parse_args()
 
     playlist_name = args.name or args.filename
@@ -46,7 +58,7 @@ def main():
     with open(args.filename) as f:
         songartists = f.readlines()
 
-    pm = PlaylistMaker()
+    pm = PlaylistMaker(args.dry_run)
     pid = pm.make_playlist(playlist_name)
     pm.populate_playlist(pid, songartists)
     pm.api.logout()
