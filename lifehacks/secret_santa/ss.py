12241a26daa2028abb65aca874f38a2ee5508259
Kathryn Lingel <kathryn@lingel.net>
2023-11-25 15:53:50 -0800

    Updates for 2022

--- lifehacks/secret_santa/ss.py
+++ lifehacks/secret_santa/ss.py
@@ -14,22 +14,22 @@ import yagmail
 gifts = 1
 
 info = {
-    'Alexa': 'alexa.keizur@gmail.com',
-    'Ben Jones': 'gilgergoggle@gmail.com',
-    'Bremmy': 'john.highwind@gmail.com',
-    'Calvin': 'marvinx03@gmail.com',
-    'Chelsea': 'chelzwa@gmail.com',
-    'Eric': 'eric.alexander.mullen@gmail.com',
-    'Jack': 'jkopakowski@gmail.com',
-    'James': 'james@dogayer.com',
-    'Janis': 'ravyn.selene@gmail.com',
-    'John': 'jtgrasel@gmail.com',
-    'Kate': 'kateevans1014@gmail.com',
-    'Kathryn': 'complikated@gmail.com',
-    'Katharina': 'ladyktob@gmail.com',
-    'Laura Grace': 'lgbeckerman@gmail.com',
-    'Ryan': 'rd.bahm@gmail.com',
-    'Sandra': 'sandralspitzer@gmail.com',
+    'Alexa': 'redacted',
+    'Ben Jones': 'redacted',
+    'Bremmy': 'redacted',
+    'Calvin': 'redacted',
+    'Chelsea': 'redacted',
+    'Eric': 'redacted',
+    'Jack': 'redacted',
+    'James': 'redacted',
+    'Janis': 'redacted',
+    'John': 'redacted',
+    'Kate': 'redacted',
+    'Kathryn': 'redacted',
+    'Katharina': 'redacted',
+    'Laura Grace': 'redacted',
+    'Ryan': 'redacted',
+    'Sandra': 'redacted',
 }
 
 
@@ -47,23 +47,27 @@ mutual_stoplist = [
     ('James', 'Janis'),
     ('Jack', 'Ben Jones'),
     ('Jack', 'Janis'),
+    ('Kathryn', 'Janis'),
 ]
 
 # populate with last year's assignments to avoid double-punching
 stoplist = {
-    'Alexa': ['Kate'],
-    'Ben Jones': ['Eric'],
-    'Calvin': ['Sandra'],
-    'Chelsea': ['Kathryn'],
-    'Eric': ['Bremmy'],
-    'John': ['Ryan'],
-    'Kate': ['Katharina'],
-    'Kathryn': ['Janis'],
-    'Katharina': ['John'],
-    'Ryan': ['Calvin'],
-    'Sandra': ['Chelsea'],
-    'Janis': ['Ben Jones'],
-    'Bremmy': ['Alexa'],
+    'Alexa': ['Chelsea'],
+    'Ben Jones': ['Bremmy'],
+    'Calvin': ['Kathryn'],
+    'Chelsea': ['Janis'],
+    'Eric': ['Laura Grace'],
+    'Jack': ['Alexa'],
+    'James': ['Kate'],
+    'Janis': ['John'],
+    'John': ['Jack'],
+    'Kate': ['James'],
+    'Kathryn': ['Eric'],
+    'Katharina': ['Calvin'],
+    'Laura Grace': ['Ryan'],
+    'Ryan': ['Ben Jones'],
+    'Sandra': ['Katharina'],
+    'Bremmy': ['Sandra'],
 }
 
 for a, b in mutual_stoplist:
@@ -114,33 +118,38 @@ def ss(n, givers, constraints):
 
 email_text = """Dear {},
 
-Happy Secret Santa 2021! You're giving a gift to {} () this year, to be exchanged on January 9, 2022. Price target is $25, give or take. Check Discord thread for wishlist links and post your own!
+Happy Secret Santa 2022! You're giving a gift to {} this year, to be exchanged on January 7, 2023. Price target is $25, give or take. Check Discord for a wishlist links thread and post your own!
 
 Happy holidays,
 Santa's Helper
 """
 
 
-def send_emails(assigns):
-    y = yagmail.SMTP('complikated@gmail.com')
+def send_emails(assigns, dry_run=True):
+    y = yagmail.SMTP('complikated@gmail.com', 'redacted')
 
     for assign, assignee in assigns.items():
         assignee = assignee[0]
-        giver_email, wishlist = info[assign]
+        giver_email = info[assign]
 
         body = email_text.format(assign, assignee)
 
-        y.send(
-            to=giver_email,
-            subject='Friendsmas Secret Santa 2021',
-            contents=body,
-        )
+        if dry_run:
+            print(body)
+        else:
+            y.send(
+                to=giver_email,
+                subject='Friendsmas Secret Santa 2022',
+                contents=body,
+            )
 
 
 
 def run():
     parser = argparse.ArgumentParser(description='Generate a Secret Santa scenario for the input people')
     parser.add_argument('--num-gifts', '-n', type=int, default=gifts, help='The number of gifts each person should give and receive (default %d)' % gifts)
+    parser.add_argument('--dry-run', '-d', action='store_true', help='A dry run will print emails instead of sending them')
+    parser.add_argument('--go-mode', '-g', action='store_true', help='Send emails without confirming assignments')
     parser.add_argument('players', nargs='*', default=list(info.keys()), help='The players. Input all names separated by spaces')
     args = parser.parse_args()
 
@@ -153,13 +162,16 @@ def run():
         print("oops; rerunning")
         answer = ss(args.num_gifts, args.players, stoplist)
 
-    print(answer)
+    if not args.go_mode:
+        print(answer)
 
-    go_ahead = input('Proceed to email? ')
-    if go_ahead[0] in 'yY':
-        send_emails(answer)
+        go_ahead = input('Proceed to email? ')
+        if go_ahead[0] in 'yY':
+            send_emails(answer, args.dry_run)
+        else:
+            run()
     else:
-        run()
+        send_emails(answer, args.dry_run)
 
 
 if __name__ == '__main__':
